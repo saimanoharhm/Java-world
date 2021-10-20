70e44625b80269e7aec44518315340466607b81d
Annam Sai Manohar <saimanohar987@gmail.com>
2021-10-20 21:40:04 +0530

    feat: Add Today's Work!

--- Java IO Streams/BufferedStreamsAndBufferedReaderAndWriter.java
+++ Java IO Streams/BufferedStreamsAndBufferedReaderAndWriter.java
@@ -1,12 +1,27 @@
 import java.io.*;
-public class BufferedStreamsAndBufferedReaderAndWriter {
+
+public class BufferedStreamsAndBufferedReaderAndWriter { 
   public static void main(String[] args) {
     try{
-      FileInputStream fis = new FileInputStream("Test.txt");
-      BufferedInputStream bis = new BufferedInputStream(fis);
+      // FileInputStream fis = new FileInputStream("Test.txt");
+      // BufferedInputStream bis = new BufferedInputStream(fis);
+      FileReader fis = new FileReader("Test.txt");
+      BufferedReader bis = new BufferedReader(fis);
 
       System.out.println("FILE: "+fis.markSupported());
       System.out.println("BUFFER: "+bis.markSupported());
+      System.out.print((char)bis.read());
+      System.out.print((char)bis.read());
+      System.out.print((char)bis.read());
+      bis.mark(10);
+      System.out.print((char)bis.read());
+      System.out.print((char)bis.read());
+      bis.reset();
+
+      System.out.print((char)bis.read());
+      System.out.print((char)bis.read());
+
+      System.out.println(" String: "+bis.readLine());
 
       fis.close();
       bis.close();
